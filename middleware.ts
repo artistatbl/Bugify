5ddc271f10789f7fe2fe617b85233ab07a0ce05e
Jean Daly <88166536+artistatbl@users.noreply.github.com>
2024-03-07 04:26:58 +0000

    update for production

--- middleware.ts
+++ middleware.ts
@@ -4,10 +4,10 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   // Define the list of protected routes
   const protectedRoutes = ['/dashboard', '/issuespage'];
-  //const isProtectedRoute = protectedRoutes.includes(request.nextUrl.pathname) || /\/issues\/\d+/.test(request.nextUrl.pathname);
+  const isProtectedRoute = protectedRoutes.includes(request.nextUrl.pathname) || /\/issues\/\d+/.test(request.nextUrl.pathname);
 
-  if (protectedRoutes.includes(request.nextUrl.pathname)) {
-    const session = request.cookies.get('next-auth.session-token');
+  if (isProtectedRoute) {
+    const session = request.cookies.get('__Secure-next-auth.session-token');
     if (!session) {
       const loginUrl = new URL('/login', request.nextUrl.origin);
       loginUrl.searchParams.append('redirected', 'true'); // Append query parameter
@@ -17,3 +17,24 @@ export function middleware(request: NextRequest) {
 
   return NextResponse.next();
 }
+
+
+// import { NextResponse } from 'next/server';
+// import type { NextRequest } from 'next/server';
+// import { useToast } from './lib/hooks/use-toast';
+
+// // List of protected routes
+// export function middleware(request: NextRequest) {
+//   const isProtectedRoute = ['/dashboard', '/issuespage', '/issues/:[id]'].includes(request.nextUrl.pathname);
+
+//   if (isProtectedRoute) {
+//       const session = request.cookies.get('next-auth.session-token');
+//       if (!session) {
+//           const loginUrl = new URL('/login', request.url);
+//           loginUrl.searchParams.append('redirected', 'true'); // Append query parameter
+//           return NextResponse.redirect(loginUrl);
+//       }
+//   }
+
+//   return NextResponse.next();
+// }
