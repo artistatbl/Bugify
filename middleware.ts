b6dc67201fdc85d5c2ea4528629ae4f2bc80ece7
Jean Daly <88166536+artistatbl@users.noreply.github.com>
2024-03-07 04:14:58 +0000

    bugs where is loops the user to sign in

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,9 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   // Define the list of protected routes
   const protectedRoutes = ['/dashboard', '/issuespage'];
-  const isProtectedRoute = protectedRoutes.includes(request.nextUrl.pathname) || /\/issues\/\d+/.test(request.nextUrl.pathname);
+  //const isProtectedRoute = protectedRoutes.includes(request.nextUrl.pathname) || /\/issues\/\d+/.test(request.nextUrl.pathname);
 
-  if (isProtectedRoute) {
+  if (protectedRoutes.includes(request.nextUrl.pathname)) {
     const session = request.cookies.get('next-auth.session-token');
     if (!session) {
       const loginUrl = new URL('/login', request.nextUrl.origin);
