2aa336d9e894722c93215f0cd26695b231712cb7
Jean Daly <88166536+artistatbl@users.noreply.github.com>
2024-03-12 00:14:35 +0000

    creating changelog

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,7 @@ export function middleware(request: NextRequest) {
   const isProtectedRoute = protectedRoutes.includes(request.nextUrl.pathname) || /\/issues\/\d+/.test(request.nextUrl.pathname);
 
   if (isProtectedRoute) {
-    const session = request.cookies.get('__Secure-next-auth.session-token'); // Use '__Secure-next-auth.session-token' instead of 'next-auth.session-token' for production
+    const session = request.cookies.get('next-auth.session-token'); // Use '__Secure-next-auth.session-token' instead of 'next-auth.session-token' for production
     if (!session) {
       const loginUrl = new URL('/login', request.nextUrl.origin);
       loginUrl.searchParams.append('redirected', 'true'); // Append query parameter
