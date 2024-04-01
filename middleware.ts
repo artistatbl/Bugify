13e4026f23768444061443729ec5b3f10ccbacc9
Jean Daly <88166536+artistatbl@users.noreply.github.com>
2024-04-01 20:59:38 +0100

    Error - Production failed

--- middleware.ts
+++ middleware.ts
@@ -2,18 +2,18 @@ import { getToken } from 'next-auth/jwt';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export async function middleware(req: NextRequest) {
+export async function middleware(request: NextRequest) {
   // Define the list of protected routes
   const protectedRoutes = ['/dashboard', '/issuespage'];
-  const isProtectedRoute = protectedRoutes.includes(req.nextUrl.pathname) || /\/issues\/\d+/.test(req.nextUrl.pathname);
+  const isProtectedRoute = protectedRoutes.includes(request.nextUrl.pathname) || /\/issues\/\d+/.test(request.nextUrl.pathname);
 
   if (isProtectedRoute) {
-   // const session = request.cookies.get('__secure-next-auth.session-token'); 
-    const token =  await  getToken({ req });
+    const session = request.cookies.get('__secure-next-auth.session-token'); 
+    // const token =  await  getToken({ req });
     
     // Use '__Secure-next-auth.session-token' instead of 'next-auth.session-token' for production?
-    if (!token) {
-      const loginUrl = new URL('/login', req.nextUrl.origin);
+    if (!session) {
+      const loginUrl = new URL('/login', request.nextUrl.origin);
       loginUrl.searchParams.append('redirected', 'true'); // Append query parameter
       return NextResponse.redirect(loginUrl);
     }
